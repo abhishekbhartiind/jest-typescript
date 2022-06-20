6881731526fa2dd65dd3b3269264ed366f7aea7c
Abhishek Bharti <abhishek.bharti@myzeller.com>
2022-06-20 20:00:08 +0530

    add more condition in testing

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -4,7 +4,9 @@ describe("Test Add Function", () => {
   it("should return 15 for add(10, 5)", () => {
     expect(add(10, 5)).toBe(15)
   })
-
+  it("Should return 20 for add(10, 10)", () => {
+    expect(add(10, 10)).toBe(20)
+  })
   it("should return 5 for add(2,3)", () => {
     expect(add(2, 3)).toBe(5);
   });
